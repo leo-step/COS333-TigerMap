2a388d5233c86072dce1f8c4fa5d9cd191fff0fc
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2022-11-03 19:42:27 -0400

    updated requirements.txt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,6 @@ import { useState, useEffect, code } from 'react';
 
 function App() {
     const [response, setResponse] = useState("Hi");
-    
-    useEffect(() => {
-      fetch("/api", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({"courseid": "014294"}),
-      })
-      .then(response => response.json())
-      .then(json => setResponse(json))
-      
-    }, []);
 
 
 
