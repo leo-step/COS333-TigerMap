ecf95a500af83c8e8cebb102b07351fba16dea7c
Bill Ao <bill.ao1950@gmail.com>
2022-11-03 18:57:28 -0400

    call api in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, code } from 'react';
 
 function App() {
+    const [response, setResponse] = useState("Hi");
+    
+    useEffect(() => {
+      fetch("/api", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({"courseid": "014294"}),
+      })
+      .then(response => response.json())
+      .then(json => setResponse(json))
+      
+    }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {response}
     </div>
   );
 }
