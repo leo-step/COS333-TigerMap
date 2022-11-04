9c67f2d558e99a9a7c49e3fba8eba9559f78a363
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2022-11-04 11:08:47 -0400

    show cos226 for demo purposes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,46 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function App() {
-    const [response, setResponse] = useState(null);
-    const [courseId, setCourseId] = useState("014294");
-    
-    useEffect(() => {
-      fetch("http://127.0.0.1:5000/api", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({"course_id": courseId}),
-      })
-      .then(response => response.json())
-      .then(json => setResponse(json))
-    }, [courseId]);
+  const [response, setResponse] = useState(null);
+  const [courseId, setCourseId] = useState("014294");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ course_id: courseId }),
+    })
+      .then((response) => response.json())
+      .then((json) => setResponse(json));
+  }, [courseId]);
 
   return (
     <div>
       <h1>TigerMap</h1>
-      <hr/>
-      <h2>{response && response.crosslistings} - {response && response.transcript_title}</h2>
+      <hr />
+      <h2>
+        {response && response.crosslistings} -{" "}
+        {response && response.transcript_title}
+      </h2>
       <p>{response && response.description}</p>
-      <p><b>Requirements:</b> {response && response.other_restrictions}</p>
-      <p><b>Prerequisites:</b> {response && response.prereqs.map(prereq => <div><a href="#">{prereq}</a><br/></div>)}</p>
-      <button onClick={() => setCourseId("001397")}>Switch</button>
-      <hr/>
+      <p>
+        <b>Requirements:</b> {response && response.other_restrictions}
+      </p>
+      <p>
+        <b>Prerequisites:</b>{" "}
+        {response &&
+          response.prereqs.map((prereq) => (
+            <div>
+              <a href="#" onClick={() => setCourseId("002054")}>
+                {prereq}
+              </a>
+              <br />
+            </div>
+          ))}
+      </p>
+      <hr />
       <h2>Details</h2>
       <pre>{JSON.stringify(response, null, 2)}</pre>
     </div>
