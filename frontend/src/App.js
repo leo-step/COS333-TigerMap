5ded1bd0f38716ae444d0a9ccf7e453c16d5f9a2
Aaliyah Sayed <aaliyah.sayed@gmail.com>
2022-11-03 20:47:59 -0400

    flask app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
-  const [response, setResponse] = useState("Hi");
+  const [getMessage, setGetMessage] = useState({})
 
+  useEffect(()=>{
+    axios.get('http://127.0.0.1:5000/a').then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
 
-
+  }, [])
   return (
     <div className="App">
-      {response}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>React + Flask Tutorial</p>
+        <div>{getMessage.status === 200 ?
+          <h3>{getMessage.data.message}</h3>
+          :
+          <h3>LOADING</h3>}</div>
+      </header>
     </div>
   );
 }
