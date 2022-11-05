c96d666df236a9810be9aa5a2a7d02342c6f5101
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2022-11-05 12:11:33 -0400

    outline database population script and make utils.py

---
+++ db/populate.py
@@ -0,0 +1,21 @@
+from utils import get_details, get_course_ids, get_prereqs, insert_details
+from terms import *
+
+'''
+
+================== TODO ==================
+
+1. choose terms to use, put into list
+2. in a loop, go from newest to oldest term,
+   getting course ids and their details.
+   maintain prereqs and course id dicts. skip over
+   seen class codes in course id dict.
+   insert details into db at the end of every term.
+3. create graph from prereq and course id dicts
+4. insert all dicts into the database (write util)
+
+'''
+
+# eco300 = get_details(FALL22, "001388")
+# print(get_prereqs(eco300))
+# insert_details([eco300])
---
+++ db/utils.py
@@ -0,0 +1,57 @@
+import requests
+import json
+import base64
+from dotenv import load_dotenv
+import os
+import re
+import pymongo
+
+load_dotenv()
+
+# refresh authorization token
+def refresh_token():
+    req = requests.post(
+        "https://api.princeton.edu:443/token",
+        data={"grant_type": "client_credentials"},
+        headers={
+            "Authorization": "Basic "
+            + base64.b64encode(
+                bytes(os.getenv('SECRET'), "utf-8")
+            ).decode("utf-8")
+        }
+    )
+    text = req.text
+    response = json.loads(text)
+    return response["access_token"]
+
+# get course details given term and course_id
+def get_details(term, course_id):
+    req = requests.get(
+        "https://api.princeton.edu:443/student-app/1.0.1/courses/details",
+        params={"fmt": "json", "term": term, "course_id": course_id},
+        headers={"Authorization": "Bearer " + refresh_token()})
+    response = json.loads(req.text)
+    details = response["course_details"]["course_detail"]
+    details["_id"] = details["course_id"]
+    return details
+
+# get a list of all course ids for a term
+def get_course_ids(term):
+    return ["015166", "002478", "015644"]
+
+# extract prerequisites (class codes) from text
+def get_prereqs(course_details):
+    prereq_text = course_details["other_restrictions"]
+    first_sentence = prereq_text.split('.')[0]
+    code_pattern = re.compile(r"[A-Z][A-Z][A-Z] \d\d\d")
+    no_space_pattern = re.compile(r"[A-Z][A-Z][A-Z]\d\d\d")
+    prereqs = code_pattern.findall(first_sentence)
+    no_space = no_space_pattern.findall(first_sentence)
+    prereqs.extend(list(map(lambda x: x[:3] + ' ' + x[3:], no_space)))
+    return prereqs
+
+# insert list of course detail documents into database
+def insert_details(details):
+    client = pymongo.MongoClient(os.getenv("DB_CONN"))
+    db = client.courses
+    db.details.insert_many(details)
