fa018c23bcfdf41a4a3f5c4482836842625e2bcb
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2022-11-09 22:53:43 -0500

    remove comments

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 from flask import Flask, request
-# import flask_restful
 from flask_cors import CORS # comment this on deployment
 import pymongo
 import os
@@ -29,17 +28,8 @@ def get_prereqs(prereq_text):
 def index():
     return app.send_static_file("index.html")
 
-# api = flask_restful.Api(app)
-
-# @app.route("/", defaults={'path':''})
-# def serve(path):
-#     return send_from_directory(app.static_folder,'index.html')
-
-# api.add_resource(HelloApiHandler, '/a')
-
 @app.route("/api")
 def api():
-    # handle case when course_id not provided or invalid like in A3
     course_id = request.args.get("course_id")
     details = db.details.find_one({"_id": course_id})
     details["prereqs"] = get_prereqs(details["other_restrictions"])
