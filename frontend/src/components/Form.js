1c93a04e9dcf9391b7d8617d2fc3b5cc7544c379
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2022-12-01 23:29:37 -0500

    emoji picker

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -4,32 +4,31 @@ import Row from "react-bootstrap/Row";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import EmojiPicker from "emoji-picker-react";
 
 function Form() {
   const [courses, setCourses] = useState([]);
+  const [emoji, setEmoji] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     const data = {
-        "title": event.target.elements.title.value,
-        "emoji": event.target.elements.emoji.value,
-        "courses": event.target.elements.courses.value
+      title: event.target.elements.title.value,
+      emoji: event.target.elements.emoji.value,
+      courses: event.target.elements.courses.value,
     };
 
     axios.post("http://127.0.0.1:5000/createtrack", data).then((response) => {
-        navigate("/track/" + response.data.id);
+      navigate("/track/" + response.data.id);
     });
   };
   return (
     <Container fluid className="m-2">
       <Row className="justify-content-center">
-        <form
-          onSubmit={handleSubmit}
-          style={{ maxWidth: "600px" }}
-        >
-          <div className="form-group">
+        <form onSubmit={handleSubmit} style={{ maxWidth: "600px" }}>
+          <div className="form-group" style={{paddingBottom: "10px"}}>
             <label htmlFor="title">Title</label>
             <input
               className="form-control"
@@ -38,15 +37,24 @@ function Form() {
               placeholder="Enter track title"
             />
           </div>
-          <div className="form-group">
+          <div className="form-group" style={{paddingBottom: "10px"}}>
             <label htmlFor="emoji">Emoji</label>
-            <input className="form-control" id="emoji" name="emoji" />
+            <h1>{emoji}</h1>
+            <EmojiPicker
+              width="100%"
+              height="400px"
+              autoFocusSearch={false}
+              suggestedEmojisMode={"recent"}
+              onEmojiClick={(event) => setEmoji(event.emoji)}
+            />
+            <input hidden readOnly className="form-control" id="emoji" name="emoji" value={emoji}/>
           </div>
-          <div className="form-group">
+          <div className="form-group" style={{paddingBottom: "10px"}}>
             <label>Add Courses</label>
 
             <input
               hidden
+              readOnly
               className="form-control"
               name="courses"
               value={JSON.stringify(courses)}
